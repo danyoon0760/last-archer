b1a492388adb2049d3a5822ed0a319f68ce1b07a
danyoon0760 <110739667+danyoon0760@users.noreply.github.com>
2026-06-07 13:19:22 +0900

    Use actual 32x64 sprite sheet with 8-direction frames

--- scripts/player_sprite.gd
+++ scripts/player_sprite.gd
@@ -1,132 +1,60 @@
-extends Node2D
+extends Sprite2D
 
-const PIXEL := 3.0
+const SHEET_BASE64 := "iVBORw0KGgoAAAANSUhEUgAAAGAAAADACAYAAAD7hGbWAAAEBklEQVR4nO3dTYiNURzH8R9NUWZiuitpFoOUNF4XSmahjIVMGlnKy0IxVopMFrKSopTyUhZIdhqJLCiTt1skhYXiIqbR1DRuysJCjQUu7ttzzvOcc889M9/P6pl5znPOuc+Z/32ec6b+RwIAAAAAAAAAAAAAAACcm2ZUatvjiZbPjxKLtX0/XTou5kfM6o5A+5p5E7bXmH7+FtMKu7s69PDVsG0/nFi4eKn1DZCkwuuXTf9HMN20YKibn+T2zRuhu5BJFBEw3j5e89zq7Wuldv992NrTp2t3rzuvN/oIaBQfN1+yGIDurg4vHZjqiIDAiABHDuzcL0k6efC41XVEgCMnL52SJB04MWB1HRHgwNaevtTXRh8BQxfvhO5Cpjek6CNg3a4NobuQidFEbNmbARXGRjU3odyMmbnScVEjWfplbOjiHe+DkCvmkguVMf38xhHQrJohAvbt3ZP6WqeroeueHisd3/321dlCWJrVSMndimxP62zr9k0/fxRrQWm+AqTGfQ3u6O/X5bNnU10b/VtQM6yGpr35UiQR0Dn8sea5fcuXq7PGuYKf7lTIEgHGA2By858tbf37Q/5rqg7ZujA4qN1btnht493hJXXPH9F9qbzM4bzHHgEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABTkHFGqVop5AurzkmSyjNq/bh3aNJkzJKk+cfWVO1D8Vb1VDqmbTvLGRcqq2Kj0lau7ljppX1nAxAqmVOjkvY9GX7upX0iIHD7mZ8Bs9pmS5K+jI3+9/vhD2+dff82wxYmtn0wbTv6vKGNStqXJTdoPdEPwMbezQ1p58y5817qjX4AiIDAiIDAYo+AKGbCaXI3S27zV9v2wbTt6OcBFwYHG9LOjv7+it+t39SbuV7jzLlJfM6E/8vIW2bF0e1SrfMOs/cemVOZHfed3qv4KVvb0UdA6JlwVsyEPfVhysyEY5f5GfBi0a+Ny1pay3bY+HAoa9Ul9dLX1+Myfb1tH0zbjv4Z8IeLN5IQon8LKpX5NFT5NuTwLajaHgK1/hvmum0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMs2Yte3xhFSZGbGatu+nS8cu8/eHVGvvgHqKtz4afX6rdDXdXR1B0tQ3Q8oyW/mrD7S4c0FiOaMBWPZmQJJUGBvV3ISyM2bmSsdFjZhUn2i8fdxJPWlNv/LN+preK31mdVvXDKecZczyKVfMJReqwlUE+kQEBGYUATUzI1bx71uQK6GfAT7bt4qA0JkRJ6Mo3oJC8/kMmlTPgPzVB0HbT7OXQRQz4dA76flsn2dAYFYDEGq3vMmMCAiMCAiMCAjMai0oVASwFvQbEeAezwAAAAAAAAAAAAAAAAB48BMI52Z3RuwqlAAAAABJRU5ErkJggg=="
 
-var facing := Vector2.DOWN
+const FRAME_COLUMNS := 3
+const FRAME_ROWS := 3
 
 func _ready() -> void:
+	texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
+	centered = true
+	hframes = FRAME_COLUMNS
+	vframes = FRAME_ROWS
 	z_index = 20
-	queue_redraw()
+	_load_texture()
+	_update_frame()
 
 func _process(_delta: float) -> void:
+	_update_frame()
+
+func _load_texture() -> void:
+	var image_bytes: PackedByteArray = Marshalls.base64_to_raw(SHEET_BASE64)
+	var image := Image.new()
+	var error := image.load_png_from_buffer(image_bytes)
+	if error != OK:
+		push_error("Player sprite sheet failed to load. Error: %s" % error)
+		return
+	texture = ImageTexture.create_from_image(image)
+
+func _update_frame() -> void:
 	var parent := get_parent()
+	var facing := Vector2.DOWN
 	if parent != null:
 		var value: Variant = parent.get("dir")
 		if value is Vector2 and value.length() > 0.01:
 			facing = value.normalized()
-	queue_redraw()
 
-func _draw() -> void:
-	var direction := get_direction_name(facing)
-	draw_character(direction)
+	var coords := _frame_coords_from_direction(facing)
+	frame = coords.y * FRAME_COLUMNS + coords.x
 
-func get_direction_name(v: Vector2) -> String:
+func _frame_coords_from_direction(v: Vector2) -> Vector2i:
 	if v.y < -0.35:
-		return "back"
+		if v.x < -0.35:
+			return Vector2i(0, 0)
+		if v.x > 0.35:
+			return Vector2i(2, 0)
+		return Vector2i(1, 0)
+
 	if v.y > 0.35:
-		return "front"
-	if v.x < 0.0:
-		return "left"
-	if v.x > 0.0:
-		return "right"
-	return "front"
+		if v.x < -0.35:
+			return Vector2i(0, 2)
+		if v.x > 0.35:
+			return Vector2i(2, 2)
+		return Vector2i(1, 1)
 
-func px(x: int, y: int, w: int, h: int, color: Color) -> void:
-	draw_rect(Rect2(Vector2(x, y) * PIXEL, Vector2(w, h) * PIXEL), color)
+	if v.x < -0.01:
+		return Vector2i(0, 1)
+	if v.x > 0.01:
+		return Vector2i(2, 1)
 
-func draw_character(direction: String) -> void:
-	var outline := Color(0.08, 0.07, 0.10)
-	var hair_dark := Color(0.73, 0.36, 0.12)
-	var hair := Color(0.95, 0.55, 0.20)
-	var hair_light := Color(1.0, 0.76, 0.28)
-	var skin := Color(1.0, 0.68, 0.55)
-	var skin_light := Color(1.0, 0.78, 0.66)
-	var green_dark := Color(0.02, 0.35, 0.21)
-	var green := Color(0.04, 0.67, 0.38)
-	var green_light := Color(0.32, 0.88, 0.55)
-	var belt := Color(0.62, 0.20, 0.11)
-	var boot := Color(0.28, 0.12, 0.09)
-	var white := Color(0.95, 0.92, 0.86)
-
-	# Draw around local origin; sprite height is roughly 32x42 visual pixels.
-	var ox := -16
-	var oy := -32
-
-	# legs / boots
-	px(ox + 10, oy + 31, 4, 7, outline)
-	px(ox + 19, oy + 31, 4, 7, outline)
-	px(ox + 11, oy + 31, 3, 6, boot)
-	px(ox + 19, oy + 31, 3, 6, boot)
-
-	# body outline
-	px(ox + 8, oy + 20, 17, 14, outline)
-	px(ox + 9, oy + 20, 15, 12, green)
-	px(ox + 9, oy + 20, 15, 3, green_light)
-	px(ox + 9, oy + 27, 15, 2, belt)
-	px(ox + 14, oy + 20, 5, 2, white)
-
-	# arms
-	px(ox + 5, oy + 22, 4, 10, outline)
-	px(ox + 24, oy + 22, 4, 10, outline)
-	px(ox + 6, oy + 23, 2, 7, skin)
-	px(ox + 25, oy + 23, 2, 7, skin)
-
-	if direction == "back":
-		draw_back_head(ox, oy, outline, hair_dark, hair, hair_light)
-	elif direction == "left":
-		draw_side_head(ox, oy, outline, hair_dark, hair, hair_light, skin, skin_light, true)
-	elif direction == "right":
-		draw_side_head(ox, oy, outline, hair_dark, hair, hair_light, skin, skin_light, false)
-	else:
-		draw_front_head(ox, oy, outline, hair_dark, hair, hair_light, skin, skin_light)
-
-func draw_front_head(ox: int, oy: int, outline: Color, hair_dark: Color, hair: Color, hair_light: Color, skin: Color, skin_light: Color) -> void:
-	# hair outline
-	px(ox + 6, oy + 3, 21, 4, outline)
-	px(ox + 3, oy + 7, 27, 12, outline)
-	px(ox + 5, oy + 18, 23, 5, outline)
-
-	# hair mass
-	px(ox + 7, oy + 4, 19, 4, hair)
-	px(ox + 5, oy + 8, 23, 9, hair)
-	px(ox + 7, oy + 17, 18, 4, hair_dark)
-	px(ox + 8, oy + 5, 5, 2, hair_light)
-	px(ox + 19, oy + 8, 5, 2, hair_light)
-
-	# face
-	px(ox + 9, oy + 12, 15, 9, skin)
-	px(ox + 10, oy + 13, 13, 4, skin_light)
-	px(ox + 11, oy + 15, 3, 4, outline)
-	px(ox + 20, oy + 15, 3, 4, outline)
-	px(ox + 15, oy + 20, 4, 1, Color(0.75, 0.32, 0.28))
-
-func draw_back_head(ox: int, oy: int, outline: Color, hair_dark: Color, hair: Color, hair_light: Color) -> void:
-	px(ox + 6, oy + 3, 21, 4, outline)
-	px(ox + 3, oy + 7, 27, 16, outline)
-	px(ox + 5, oy + 22, 23, 4, outline)
-	px(ox + 7, oy + 4, 19, 4, hair)
-	px(ox + 5, oy + 8, 23, 13, hair)
-	px(ox + 7, oy + 20, 18, 4, hair_dark)
-	px(ox + 9, oy + 6, 5, 2, hair_light)
-	px(ox + 21, oy + 11, 3, 4, hair_light)
-
-func draw_side_head(ox: int, oy: int, outline: Color, hair_dark: Color, hair: Color, hair_light: Color, skin: Color, skin_light: Color, left: bool) -> void:
-	var sx := -1 if left else 1
-	var face_x := ox + 8 if left else ox + 16
-	var hair_x := ox + 5 if left else ox + 6
-
-	px(hair_x, oy + 4, 22, 5, outline)
-	px(hair_x - 2 if left else hair_x, oy + 8, 24, 14, outline)
-	px(hair_x + 1, oy + 5, 19, 5, hair)
-	px(hair_x, oy + 10, 20, 10, hair)
-	px(hair_x + 2, oy + 19, 16, 4, hair_dark)
-	px(hair_x + 4, oy + 6, 5, 2, hair_light)
-
-	px(face_x, oy + 13, 9, 8, skin)
-	px(face_x + 1, oy + 14, 7, 3, skin_light)
-	if left:
-		px(face_x + 1, oy + 16, 3, 4, outline)
-		px(face_x - 2, oy + 17, 3, 3, skin)
-	else:
-		px(face_x + 5, oy + 16, 3, 4, outline)
-		px(face_x + 8, oy + 17, 3, 3, skin)
+	return Vector2i(1, 1)
